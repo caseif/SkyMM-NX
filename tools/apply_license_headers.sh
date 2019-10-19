ab1acda82d01ac194a166abbb88d40129c4cfd34
Max Roncace <me@caseif.net>
2019-10-19 16:15:12 -0400

    Update license headers

--- tools/apply_license_headers.sh
+++ tools/apply_license_headers.sh
@@ -46,25 +46,22 @@ find "./include" "./src" -type f \( -iname "*.c" -or -iname "*.h" -or -iname "*.
         # with a neat trick to deal with a multi-line context
         # we also append a dot to preserve trailing whitespace during substitution
         output=$(gawk -v RS='^$' -v hdr="$header\n\n" '{sub(/\/\*.*?Copyright \(c\).*?\*\/\n*/,hdr)}1 {print $0}' $file && echo .)
-
-        # escape any formatting sequences present in the code since printf likes to convert them
-        output="${output//%/%%}"
-        # escape previously-escaped newlines since printf likes to convert them too
-        output="${output//\\n/\\\\n}"
-        # print appends a newline, so we need to remove it and the trailing dot we added
-        output="${output:0:-2}"
-        # finally, write it to disk!
-        printf "$output" > $file
     else
         echo "Generating new header for file $file"
         
         # save output to intermediate variable with trailing character
         # this prevents bash from stripping trailing newlines
-        output="$header\n\n$(cat $file && echo .)"
-        
-        # remove the last character and write the output back to the file
-        printf "${output:0:-1}" > $file
+        output="$header"$'\n'$'\n'"$(cat $file && echo .)"
     fi
+
+    # escape any formatting sequences present in the code since printf likes to convert them
+    output="${output//%/%%}"
+    # escape previously-escaped newlines since printf likes to convert them too
+    output="${output//\\n/\\\\n}"
+    # remove the trailing dot we added
+    output="${output:0:-1}"
+    # finally, write it to disk!
+    printf "$output" > $file
 done
 
 # if we didn't update anything, say so (to avoid zero-output)
