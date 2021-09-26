b7bf4545fd1695daf71eef253461540de7956de6
Witherking25 <witherking@withertech.com>
2021-09-25 20:09:44 -0500

    added gui

---
+++ include/main.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <borealis.hpp>
+#include "console_helper.hpp"
+#include "error_defs.hpp"
+#include "frame_root.hpp"
+#include "ini_helper.hpp"
+#include "mod.hpp"
+#include "path_helper.hpp"
+#include "string_helper.hpp"
+
+#include <inipp/inipp.h>
+#include <switch.h>
+
+#include <filesystem>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <cstdio>
+#include <dirent.h>
+
+using namespace brls::i18n::literals;
+#define Q(x) #x
+#define QUOTE(x) Q(x)
+#define VERSION QUOTE(__VERSION)
+
+extern bool g_dirty;
+extern bool g_dirty_warned;
+
+extern std::string g_status_msg;
+extern bool g_tmp_status;
+
+extern ModList g_mod_list_tmp;
+
+extern std::string g_plugins_header;
+
+extern int g_scroll_dir;
+extern u64 g_last_scroll_time;
+extern bool g_scroll_initial_cooldown;
+
+extern bool g_edit_load_order;
+extern bool g_plugin;
+
+extern std::string g_sel_mod;
+extern std::string g_prev_mod;
+
+extern bool g_dialog_open;
+
+void clearTempEffects(void);
+u64 _nanotime(void);
--- src/path_helper.cpp
+++ src/path_helper.cpp
@@ -5,53 +5,68 @@
 
 #include <string>
 
-#define SPL_CONFIG_EXO_VERSION ((SplConfigItem) 65000)
+#define SPL_CONFIG_EXO_VERSION ((SplConfigItem)65000)
 
 static bool initted = false;
 static bool newRomfsPath = false;
 
-static void _init(void) {
-    splInitialize();
-    u64 ver = 0;
-    splGetConfig(SPL_CONFIG_EXO_VERSION, &ver);
-    splExit();
+static void _init(void)
+{
+	splInitialize();
+	u64 ver = 0;
+	splGetConfig(SPL_CONFIG_EXO_VERSION, &ver);
+	splExit();
 
-    u32 exoMajor = (ver >> 56) & 0xFF;
-    u32 exoMinor = (ver >> 48) & 0xFF;
-    u32 exoMicro = (ver >> 40) & 0xFF;
+	u32 exoMajor = (ver >> 56) & 0xFF;
+	u32 exoMinor = (ver >> 48) & 0xFF;
+	u32 exoMicro = (ver >> 40) & 0xFF;
 
-    // AMS 0.10.0 changed the RomFS directory
-    if (exoMajor > 0 || (exoMinor >= 10)) {
-        newRomfsPath = true;
-    } else {
-        newRomfsPath = false;
-    }
+	// AMS 0.10.0 changed the RomFS directory
+	if (exoMajor > 0 || (exoMinor >= 10))
+	{
+		newRomfsPath = true;
+	}
+	else
+	{
+		newRomfsPath = false;
+	}
 }
 
-const char *getBaseRomfsPath(void) {
-    if (!initted) {
-        _init();
-    }
+const char *getBaseRomfsPath(void)
+{
+	if (!initted)
+	{
+		_init();
+	}
 
-    if (newRomfsPath) {
-        return SKYRIM_ROMFS_DIR;
-    } else {
-        return SKYRIM_ROMFS_DIR_OLD;
-    }
+	if (newRomfsPath)
+	{
+		return SKYRIM_ROMFS_DIR;
+	}
+	else
+	{
+		return SKYRIM_ROMFS_DIR_OLD;
+	}
 }
 
-std::string getRomfsPath(std::string &partial) {
-    return getRomfsPath(partial.c_str());
+std::string getRomfsPath(std::string &partial)
+{
+	return getRomfsPath(partial.c_str());
 }
 
-std::string getRomfsPath(const char *partial) {
-    if (!initted) {
-        _init();
-    }
+std::string getRomfsPath(const char *partial)
+{
+	if (!initted)
+	{
+		_init();
+	}
 
-    if (newRomfsPath) {
-        return std::string(SKYRIM_ROMFS_DIR) + "/" + partial;
-    } else {
-        return std::string(SKYRIM_ROMFS_DIR_OLD) + "/" + partial;
-    }
+	if (newRomfsPath)
+	{
+		return std::string(SKYRIM_ROMFS_DIR) + "/" + partial;
+	}
+	else
+	{
+		return std::string(SKYRIM_ROMFS_DIR_OLD) + "/" + partial;
+	}
 }
